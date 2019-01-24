3264010cfc1ca1d00a2b6274ec7dda1e92f4c412
Mahmoud Elmokhtar <mahmoud@birdsol.com>
2019-01-24 09:55:38 +0200

    Basic laravel package setup

---
+++ src/Providers/RespondServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BirdSolutions\Respond\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Setup the resource publishing groups.
+     */
+    protected function offerPublishing()
+    {
+       //
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace BirdSolutions\Respond\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+    /**
+     * Setup the test case.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Tear down the test case.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Get the service providers for the package.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'BirdSolutions\Respond\Providers\ResponseServiceProvider',
+        ];
+    }
+
+    /**
+     * Configure the environment.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        foreach ($this->extraConfigs() as $key => $value) {
+            $app['config']->set($key, $value);
+        }
+    }
+
+    /**
+     * Override this method to set configuration values in your test class.
+     *
+     * @return array of config keys (in dot-notation) and values
+     */
+    protected function extraConfigs(): array
+    {
+        return [
+            'app.debug' => true,
+        ];
+    }
+
+    /**
+     * Run the migrations registered in the app.
+     */
+    protected function loadRegisteredMigrations()
+    {
+        $this->artisan('migrate');
+
+        $this->beforeApplicationDestroyed(function () {
+            $this->artisan('migrate:rollback');
+        });
+    }
+}
